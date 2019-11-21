6c0983711730eea0b4b5e6deba139a4503897004
DevMaster15 <andreamaestro98@gmail.com>
2019-11-21 11:56:14 +0100

    Sostituzione ricorsione e while

--- ProgettoASD/Calcolo/src/CalcoloTempi.java
+++ ProgettoASD/Calcolo/src/CalcoloTempi.java
@@ -176,37 +176,73 @@ public class CalcoloTempi {
         return e;
     }
 
-    public static void swap(double array[], int i, int j) {
-        double temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
 
-    public static double add(double array[], int left, int right) {
-        double somma = 0;
-
-        for (int i = left; i <= right; i++) {
-            somma += array[i];
-        }
-
-        return somma;
-    }
-
-
-    /*Prende in input un intero left che indica l'indice di inizio
-    dell'array/sottoarray, e un right per la fine, e un array di double.
-    Divide l'array in gruppi di 5 elementi ciascuno, e ne calcola la mediana.
-    Ricorsivamente poi calcola la mediana delle mediane:
-    1)  A intervalli di 5 in 5, applica median ai sottoarray, calcolandone
-        le mediane
-    2)  Le raggruppa tramite swap, a sinistra dell'array.
-    3)  Quando ha trovato le n/5 mediane, calcola ricorsivamente le medinae
-        sul sottoarray di inizi left e fine count.
-    4)  Quando il gruppo di mediane a inizio array è minore o uguale a 5,
-        ne calcola la mediana e ne restituisce l'indice.
-
+    /**
+     * Start è la funzione da cui parte l'intera procedura
+     * c'è la prima chiamata a pivot e partition per sistemare l'array attorno ad un valore
+     * le somme degli elementi a sinistra e destra del pivot
+     * la somma di tutti gli elementi
+     *
+     * e la chiamata alla procedura medianaPesata
+     *
+     * @param array di valori double
+     * @return 0 se la sommaTotale (di tutti gli elementi = 0)
+     * @return il risultato della procedua medianaPesata() che sarà la mediana pesata inferiore
      */
 
+    public static double start(double array[]) {
+        int left = 0;
+        int right = array.length - 1;
+        int p = pivot(array, left, right);
+        int pivotIndex = partition(array, left, right, p);
+
+        double sommaLeftPivot = add(array, 0, pivotIndex - 1); //somma da  fino all'elemento
+        double sommaRightPivot = add(array, pivotIndex + 1, array.length - 1); //ok
+        double sommaTotale = sommaRightPivot + sommaLeftPivot + array[pivotIndex];
+        double sommaMezzi = sommaTotale/2;
+
+        if(sommaTotale == 0)
+            return 0;
+        else{
+            while(!(sommaLeftPivot < sommaMezzi && sommaRightPivot <= sommaMezzi)){
+                if(sommaLeftPivot >= sommaMezzi){
+                    right = pivotIndex;
+                    p = pivot(array, left, right);
+                    pivotIndex = partition(array, left, right, p);
+                    sommaRightPivot = sommaRightPivot + add(array, pivotIndex + 1, right);
+                    sommaLeftPivot = sommaTotale - sommaRightPivot - array[pivotIndex];
+                } else {
+                    left = pivotIndex;
+                    p = pivot(array, left, right);
+                    pivotIndex = partition(array, left, right, p);
+                    sommaLeftPivot = sommaLeftPivot + add(array, left, pivotIndex - 1);
+                    sommaRightPivot = sommaTotale - sommaLeftPivot - array[pivotIndex];
+                }
+            }
+
+        }
+
+        return array[pivotIndex];
+    }
+
+    /**
+     * Prende in input un intero left che indica l'indice di inizio
+     *     dell'array/sottoarray, e un right per la fine, e un array di double.
+     *     Divide l'array in gruppi di 5 elementi ciascuno, e ne calcola la mediana.
+     *     Ricorsivamente poi calcola la mediana delle mediane:
+     *     1)  A intervalli di 5 in 5, applica median ai sottoarray, calcolandone
+     *         le mediane
+     *     2)  Le raggruppa tramite swap, a sinistra dell'array.
+     *     3)  Quando ha trovato le n/5 mediane, calcola ricorsivamente le medinae
+     *         sul sottoarray di inizi left e fine count.
+     *     4)  Quando il gruppo di mediane a inizio array è minore o uguale a 5,
+     *         ne calcola la mediana e ne restituisce l'indice.
+     *
+     * @param array
+     * @param left
+     * @param right
+     * @return
+     */
 
     private static int pivot(double array[], int left, int right) {
 
@@ -232,6 +268,18 @@ public class CalcoloTempi {
         return pivot(array, left, count);
     }
 
+    /**
+     * prende come parametri l'array di double e gli indice left e right che indicano rispettivamente
+     * l'indice più a sinistra e più a destra dell'array/sottoarray, e l'indice p che è l'indice del pivot
+     *
+     * questa procedura "divide" l'array in tre parti da left
+     *
+     * @param array
+     * @param left
+     * @param right
+     * @param p
+     * @return
+     */
 
     public static int partition(double[] array, int left, int right, int p) {
         int i = left;
@@ -271,41 +319,20 @@ public class CalcoloTempi {
         return median;
     }
 
-    public static double start(double array[]) {
-        int p = pivot(array, 0, array.length - 1);
-        int index = partition(array, 0, array.length - 1, p);
-
-        double sommaLeftPivot = add(array, 0, index - 1); //ok
-        double sommaRightPivot = add(array, index + 1, array.length - 1); //ok
-        double sommaTotale = sommaRightPivot + sommaLeftPivot + array[index];
-
-        if(sommaTotale == 0)
-            return 0;
-        else
-            return medianaPesata(array, 0, array.length - 1, sommaTotale, sommaLeftPivot, sommaRightPivot, index, sommaTotale / 2);
+    public static void swap(double array[], int i, int j) {
+        double temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 
-    public static double medianaPesata(double array[], int left, int right, double sommaTotale, double sommaLeftPivot,
-                                       double sommaRightPivot, int index, double sommaMezzi) {
+    public static double add(double array[], int left, int right) {
+        double somma = 0;
 
-        if (sommaLeftPivot < sommaMezzi && sommaRightPivot <= sommaMezzi) {
-            return array[index];
-        } else if (sommaLeftPivot >= sommaMezzi) {
-            int tmpright = index;
-            int p = pivot(array, left, tmpright);
-            int tmpindex = partition(array, left, tmpright, p);
-            double tmpsumRight = sommaRightPivot + add(array, tmpindex + 1, tmpright);
-            double tmpsumLeft = sommaTotale - tmpsumRight - array[tmpindex];
-            return medianaPesata(array, left, tmpright, sommaTotale, tmpsumLeft, tmpsumRight, tmpindex, sommaMezzi);
-        } else {
-            int tmpleft = index;
-            int p = pivot(array, tmpleft, right);
-            int tmpindex = partition(array, tmpleft, right, p);
-            double tmpsumLeft = sommaLeftPivot + add(array, tmpleft, tmpindex - 1);
-            double tmpsumRight = sommaTotale - tmpsumLeft - array[tmpindex];
-            return medianaPesata(array, tmpleft, right, sommaTotale, tmpsumLeft, tmpsumRight, tmpindex, sommaMezzi);
+        for (int i = left; i <= right; i++) {
+            somma += array[i];
         }
 
-
+        return somma;
     }
+
 }
